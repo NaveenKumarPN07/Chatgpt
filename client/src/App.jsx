db9a037b514224f07d6308594812c19fa9fe5eb0
naveen <naveenkumarpn50@gmail.com>
2026-01-17 23:06:42 +0530

    completed sidebar of frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ChatBox from "./components/ChatBox";
+import Sidebar from "./components/Sidebar";
+import Credits from "./pages/Credits";
+import Login from "./pages/Login";
+import Community from "./pages/Community";
+
+const App = () => {
+  return (
+    <>
+    <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+      <div className="flex h-screen w-screen">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<ChatBox />} />
+          <Route path="/credits" element={<Credits/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/community" element={<Community/>}/>
+        </Routes>
+      </div>
+    </div>
+      
+    </>
+  );
+};
+
+export default App;
